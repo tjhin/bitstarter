ca850d1d23e9e24a5d93a5caef6f1cb12e5d690c
tjhin <tjhindr.01b@gmail.com>
2013-07-29 18:34:00 +0000

    read from file test

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var Buffer = require('buffer');
+
+
+//var fileread = fs.readFileSync(index.html);
+var buf = new Buffer(fs.readFileSync(index.html));
+
+//var buffer = new Buffer(8);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.tosString);
 });
 
 var port = process.env.PORT || 5000;
